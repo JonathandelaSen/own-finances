5029b9d212637c5841322a65db6bb6707b108dde
JonathandelaSen <jonathandelasen@gmail.com>
2025-01-05 19:30:48 +0100

    added sorting to dividends table

--- src/sections/dividends/components/dividends-table/dividends-table.tsx
+++ src/sections/dividends/components/dividends-table/dividends-table.tsx
@@ -3,6 +3,7 @@ import {
   getCoreRowModel,
   flexRender,
   useReactTable,
+  getSortedRowModel,
 } from "@tanstack/react-table"
 import React from "react"
 import { CompanyDividend } from "@modules/dividends/domain/company-dividend"
@@ -50,22 +51,47 @@ export const DividendsTable: FC<Props> = (params: Props) => {
     data: params.dividends,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
   })
   return (
     <table>
       <thead>
         {table.getHeaderGroups().map((headerGroup) => (
           <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <th key={header.id} className={styles.columnHeader}>
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-              </th>
-            ))}
+            {headerGroup.headers.map((header) => {
+              return (
+                <th key={header.id} colSpan={header.colSpan}>
+                  {header.isPlaceholder ? null : (
+                    <div
+                      className={
+                        header.column.getCanSort()
+                          ? "cursor-pointer select-none"
+                          : ""
+                      }
+                      onClick={header.column.getToggleSortingHandler()}
+                      title={
+                        header.column.getCanSort()
+                          ? header.column.getNextSortingOrder() === "asc"
+                            ? "Sort ascending"
+                            : header.column.getNextSortingOrder() === "desc"
+                            ? "Sort descending"
+                            : "Clear sort"
+                          : undefined
+                      }
+                    >
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                      {{
+                        asc: " 🔼",
+                        desc: " 🔽",
+                      }[header.column.getIsSorted() as string] ?? null}
+                    </div>
+                  )}
+                </th>
+              )
+            })}
           </tr>
         ))}
       </thead>
